64bb7d3aa36bcad66b82e584f24acc4142794fd4
José Galinha <jbgalinha@gmail.com>
2021-06-19 15:32:25 +0100

    :construction: WIP(twam.estig): update angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { RouterModule, Routes } from '@angular/router';
+import { PaginaInicialComponent } from './pagina-inicial/pagina-inicial.component';
+import { PesquisarReceitasComponent } from './pesquisar-receitas/pesquisar-receitas.component';
+import { ListarReceitasComponent } from './listar-receitas/listar-receitas.component';
+import { ReceitasFavoritasComponent } from './receitas-favoritas/receitas-favoritas.component';
 
+const appRoutes: Routes = [
+    {path: 'pagina-inicial', component: PaginaInicialComponent},
+    {path: 'pesquisar-receitas', component: PesquisarReceitasComponent},
+    {path: 'listar-receitas', component: ListarReceitasComponent},
+    {path: 'receitas-favoritas', component: ReceitasFavoritasComponent},
+    {path: '', redirectTo: 'pagina-inicial', pathMatch: 'full'},
+    {path: '**', component: PaginaInicialComponent}
+  ];
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  declarations: [AppComponent, HeaderComponent, PaginaInicialComponent, PesquisarReceitasComponent, ListarReceitasComponent, ReceitasFavoritasComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [Title],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  name = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor(private router: Router) {
+    }
+  ngOnInit(): void {}
 }
